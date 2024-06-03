06aa309fdad7aea7ad0813f27df7793b35bbb865
lucasphn <almeida_solucoes@outlook.com.br>
2024-06-03 20:44:13 -0300

    Desenvolvendo validação schema com pandera

---
+++ app/inf_schema.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import pandera as pa
+
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# from schema import ProdutoSchema, ProductSchemaKPI
+
+def load_settings():
+    """Carrega as configurações a partir de variáveis de ambiente."""
+    dotenv_path = Path.cwd() / '.env'
+    load_dotenv(dotenv_path=dotenv_path)
+
+    settings = {
+        "db_host": os.getenv("POSTGRES_HOST"),
+        "db_user": os.getenv("POSTGRES_USER"),
+        "db_pass": os.getenv("POSTGRES_PASSWORD"),
+        "db_name": os.getenv("POSTGRES_DB"),
+        "db_port": os.getenv("POSTGRES_PORT"),
+    }
+
+    return settings
+
+def extrair_do_sql(query: str) -> pd.DataFrame:
+    """
+    Extrai dados do banco de dados SQL usando a consulta fornecida.
+
+    Args:
+        query: A consulta SQL para extrair dados.
+
+    Returns:
+        Um DataFrame do Pandas contendo os dados extraídos.
+    """
+    settings = load_settings()
+
+    # Criar a string de conexão com base nas configurações
+    connection_string = f"postgresql://{settings['db_user']}:{settings['db_pass']}@{settings['db_host']}:{settings['db_port']}/{settings['db_name']}"
+
+    # Criar engine de conexão
+    engine = create_engine(connection_string)
+
+    with engine.connect() as conn, conn.begin():
+            df_crm = pd.read_sql(query, conn)
+
+    return df_crm
+
+if __name__ == '__main__':
+     query = 'SELECT * FROM produtos_bronze'
+     df_crm = extrair_do_sql(query=query)
+     
+     # inferindo schema com pandera
+     schema_crm = pa.infer_schema(df_crm)
+
+    # criando um arquivo com o nosso esquema que o pandera criou, e que está armazenado na variável acima 'schema_crm'
+     with open('schema_crm.py', 'w', encoding='utf-8') as arquivo:
+          arquivo.write(schema_crm.to_script())
+
+     print(schema_crm)
---
+++ app/schema_crm.py
@@ -0,0 +1,36 @@
+import pandera as pa
+from pandera.typing import Series
+
+email_regex = r"[^@]+@[^@]+\.[^@]+"
+
+class ProdutoSchema(pa.SchemaModel):
+    """
+    Define o esquema para a validação de dados de produtos com Pandera.
+    
+    Este esquema inclui campos básicos para produtos, incluindo um campo de e-mail
+    validado por uma expressão regular.
+
+    Attributes:
+        id_produto (Series[int]): Identificador do produto.
+        nome (Series[str]): Nome do produto.
+        quantidade (Series[int]): Quantidade disponível do produto, deve estar entre 1 e 2000.
+        preco (Series[float]): Preço do produto, deve ser maior que 0.
+        categoria (Series[str]): Categoria do produto.
+        email (Series[str]): E-mail associado ao produto, deve seguir o formato padrão de e-mails.
+    """
+    id_produto: Series[int]
+    nome: Series[str]
+    quantidade: Series[int] = pa.Field(ge=1, le=2000)
+    preco: Series[float] = pa.Field(gt=0)
+    categoria: Series[str]
+    # email: Series[str] = pa.Field(regex=email_regex)
+
+    class Config:
+        coerce = True # garantir que os tipos de dados estejam iguais ao nosso schema
+        strict = True # garantir que os meus dados estejam com a mesma quantidade de colunas do schema
+
+class ProductSchemaKPI(ProdutoSchema):
+
+    valor_total_estoque: Series[float] = pa.Field(ge=0)  # O valor total em estoque deve ser >= 0
+    categoria_normalizada: Series[str]  # Assume-se que a categoria será uma string, não precisa de check específico além de ser uma string
+    disponibilidade: Series[bool]  # Disponibilidade é um booleano, então não precisa de check específico
